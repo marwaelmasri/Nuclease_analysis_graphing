62dc810d02af49ec91b4cba052ded52cf1c4b742
marwaelmasri <54397244+marwaelmasri@users.noreply.github.com>
2025-10-23 11:35:41 +0100

    Add files via upload

--- analyse-nuclease.R
+++ analyse-nuclease.R
@@ -14,9 +14,9 @@ library(dplyr)
 
 
 #set working directory
-setwd( #add your working directory here, use " " and use / not \
-  # eg setwd("C:/Users/documents/mydata")
-  )
+#add your working directory here, use " " and use / not \
+# eg setwd("C:/Users/documents/mydata")
+setwd("C:/Users/marwa/OneDrive - University College London/Documents/Nuclease Assay/SEQ/2025-10-21")
 
 
 #open file, add your own file name
@@ -27,8 +27,12 @@ uncut <- peaks %>%
   filter(Size >= 45 & Size <= 55)
 
 #filter cut peak
-cut <- peaks %>%
-  filter(Size >=27 & Size <= 37)
+cut_endo <- peaks %>%
+  filter(Size >=20 & Size <= 37)
+
+#filter cut peak
+cut_exo <- peaks %>%
+  filter(Size <=13)
 
 #sum area of uncut peak for each trace
 uncut_areasum <- uncut %>% 
@@ -39,15 +43,23 @@ uncut_areasum <- uncut %>%
 #uncut_areasum <- uncut_areasum[-60,]
 
 #sum area of cut peak for each trace
-cut_areasum <- cut %>% 
+cutendo_areasum <- cut_endo %>% 
   group_by(`Sample File Name`) %>% 
-  summarise(peak_area_cut = sum(Area))
+  summarise(peak_area_cut_endo = sum(Area))
+
+#sum area of cut peak for each trace
+cutexo_areasum <- cut_exo %>% 
+  group_by(`Sample File Name`) %>% 
+  summarise(peak_area_cut_exo = sum(Area))
 
 #join cut and uncut tables
-percent_cut <- cbind(cut_areasum, uncut_areasum)
+percent_cut <- cbind(cutendo_areasum, cutexo_areasum, uncut_areasum)
 
-#add percent cut column
-percent_cut$peak_percent_cut <- (percent_cut$peak_area_cut / percent_cut$peak_area_uncut)*100
+#add percent cut column FOR ENDONUCLEASE
+percent_cut$peak_endo_percent_cut <- (percent_cut$peak_area_cut_endo / percent_cut$peak_area_uncut)*100
+
+#add percent cut column FOR EXONUCLEASE
+percent_cut$peak_exo_percent_cut <- (percent_cut$peak_area_cut_exo / percent_cut$peak_area_uncut)*100
 
 #export as csv
 write.csv(percent_cut, "percent_cut.csv", row.names = FALSE)
