ce0e9e0e59b1ddf77381f47d82a1eba408c6c2fc
marwaelmasri <54397244+marwaelmasri@users.noreply.github.com>
2025-12-02 11:35:52 +0000

    Update analyse-nuclease.R
    updated to full join and fixed formulae, changed to read txt instead of excel

--- analyse-nuclease.R
+++ analyse-nuclease.R
@@ -7,11 +7,10 @@
 
 
 #install packages and load libraries
-install.packages("readxl")
-install.packages("tidyverse")
-library(readxl)
-library(dplyr)
 
+install.packages("tidyverse")
+library(dplyr)
+library(readr)
 
 #set working directory
 #add your working directory here, use " " and use / not \
@@ -20,18 +19,20 @@ setwd("")
 
 
 #open file, add your own file name
-peaks <- read_excel("peaks.xlsx")
+peaks_uncut <- read_tsv("uncut.txt")
+peaks_cut <- read_tsv("cut.txt")
+
 
 #filter uncut peak
-uncut <- peaks %>%
+uncut <- peaks_uncut %>%
   filter(Size >= 45 & Size <= 55)
 
 #filter cut peak
-cut_endo <- peaks %>%
+cut_endo <- peaks_cut %>%
   filter(Size >=20 & Size <= 37)
 
 #filter cut peak
-cut_exo <- peaks %>%
+cut_exo <- peaks_cut %>%
   filter(Size <=13)
 
 #sum area of uncut peak for each trace
@@ -39,8 +40,6 @@ uncut_areasum <- uncut %>%
   group_by(`Sample File Name`) %>% 
   summarise(peak_area_uncut = sum(Area))
 
-#if you have to delete any rows
-#uncut_areasum <- uncut_areasum[-60,]
 
 #sum area of cut peak for each trace
 cutendo_areasum <- cut_endo %>% 
@@ -53,16 +52,21 @@ cutexo_areasum <- cut_exo %>%
   summarise(peak_area_cut_exo = sum(Area))
 
 #join cut and uncut tables
-percent_cut <- cbind(cutendo_areasum, cutexo_areasum, uncut_areasum)
+percent_cut <- full_join(uncut_areasum, cutendo_areasum, by = "Sample File Name")
+percent_cut <- full_join(percent_cut, cutexo_areasum, by = "Sample File Name")
+
+# change na to 0
+percent_cut[is.na(percent_cut)] <- 0
 
 #add percent cut column FOR ENDONUCLEASE
-percent_cut$peak_endo_percent_cut <- (percent_cut$peak_area_cut_endo / (percent_cut$peak_area_cut_endo/percent_cut$peak_area_uncut))*100
+percent_cut$peak_endo_percent_cut <- (percent_cut$peak_area_cut_endo / (percent_cut$peak_area_cut_endo+percent_cut$peak_area_uncut))*100
 
 #add percent cut column FOR EXONUCLEASE
-percent_cut$peak_exo_percent_cut <- (percent_cut$peak_area_cut_exo / (percent_cut$peak_area_cut_exo/percent_cut$peak_area_uncut))*100
+percent_cut$peak_exo_percent_cut <- (percent_cut$peak_area_cut_exo / (percent_cut$peak_area_cut_exo+percent_cut$peak_area_uncut))*100
 
 #export as csv
 write.csv(percent_cut, "percent_cut.csv", row.names = FALSE)
+
 
 
 
